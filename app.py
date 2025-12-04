b3397a78ca304ccef98f07fdc1b2ab2240880dc3
Remaag <remaag0202@gmail.com>
2025-12-04 09:26:13 -0300

    Funcionouuuu

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from views.api_routes import api_bp
 from seed import seed_data
 from models import Animal, Ong, Usuario, PedidoAdocao
 
+
 def cleanup_development_environment():
     """Remove o DB e o conteúdo dos uploads para um estado limpo de desenvolvimento."""
     db_path = Config.DATABASE_PATH
@@ -21,7 +22,7 @@ def cleanup_development_environment():
     # 2. Remove a pasta de uploads se ela existir
     if os.path.exists(upload_dir):
         # Remove a pasta e todo o seu conteúdo
-        shutil.rmtree(upload_dir) 
+        shutil.rmtree(upload_dir)
 
     # 3. CRIAÇÃO GARANTIDA DE DIRETÓRIOS (O FIX)
     # Garante que a pasta instance exista
@@ -32,16 +33,14 @@ def cleanup_development_environment():
         os.makedirs(upload_dir)
 
 
-
-
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-
     db.init_app(app)
 
-    instance_path = os.path.dirname(app.config['SQLALCHEMY_DATABASE_URI'].replace('sqlite:///', ''))
+    instance_path = os.path.dirname(
+        app.config['SQLALCHEMY_DATABASE_URI'].replace('sqlite:///', ''))
     upload_path = app.config['UPLOAD_FOLDER']
 
     if not os.path.exists(instance_path):
@@ -56,8 +55,6 @@ def create_app(config_class=Config):
     return app
 
 
-
-
 if __name__ == '__main__':
     is_reloader = os.environ.get('WERKZEUG_RUN_MAIN') == 'true'
 
@@ -67,17 +64,17 @@ if __name__ == '__main__':
     app = create_app()
 
     login_manager.init_app(app)
-    
+
     @login_manager.user_loader
     def load_user(user_id):
         ong = db.session.get(Ong, int(user_id))
         if ong:
-            return ong     
+            return ong
         return None
-    
+
     if not is_reloader:
         with app.app_context():
             db.create_all()
-            seed_data(app)
-    
+            # seed_data(app)
+
     app.run(debug=True)
